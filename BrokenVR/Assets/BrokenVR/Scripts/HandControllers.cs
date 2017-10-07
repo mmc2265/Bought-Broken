388b8e92e81e6d258d6eb255f3bc19da3fc4fb9e
BrokenVR <tejasshroff24@gmail.com>
2017-10-07 17:39:40 -0400

    First commit yall

---
+++ BrokenVR/Assets/BrokenVR/Scripts/HandControllers.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandControllers : MonoBehaviour {
+
+	public SteamVR_TrackedObject trackedObj;
+    public SteamVR_Controller.Device device;
+
+    private Rigidbody rb;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		device = SteamVR_Controller.Input ((int)trackedObj.index);
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "BrokenPiece")
+        {
+            if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                GrabObject(other);
+            }
+        }
+
+        if (other.gameObject.tag == "BrokenPiece")
+        {
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                ReleaseObject(other);
+            }
+        }
+    }
+
+
+    public void GrabObject(Collider coli)
+    {
+        coli.gameObject.transform.SetParent(gameObject.transform);
+
+        Rigidbody coliRb = coli.gameObject.GetComponent<Rigidbody>();
+        coliRb.isKinematic = true;
+        coliRb.useGravity = false;
+    }
+
+    public void ReleaseObject(Collider coli)
+    {
+        coli.gameObject.transform.SetParent(null);
+
+        Rigidbody coliRb = coli.gameObject.GetComponent<Rigidbody>();
+        coliRb.isKinematic = false;
+        coliRb.useGravity = true;
+        coliRb.velocity = device.velocity;
+        Debug.Log(rb.velocity);
+        coliRb.angularVelocity = device.angularVelocity;
+    }
+}
---
+++ BrokenVR/Assets/BrokenVR/Scripts/VaseCreate.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaseCreate : MonoBehaviour {
+
+	public GameObject rightController;
+    public Vector3 upDirection;
+    public GameObject staticVaseBase;
+    //public SteamVR_Controller.Device device;
+
+    public float angle;
+
+	public GameObject brokenVase;
+	public GameObject cylinder;
+
+	// Use this for initialization
+	void Start () {
+        staticVaseBase.transform.forward = upDirection;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //device = SteamVR_Controller.Input ((int)trackedObj.index);
+       
+       
+	}
+
+	private void OnTriggerStay(Collider col) {
+        if (upDirection == transform.forward)
+        {
+            transform.position = staticVaseBase.transform.position;
+        }
+
+        /*if (col.gameObject.tag == "BrokenVase") 
+		{
+            cylinder.transform.position = rightController.transform.position;
+            cylinder.transform.rotation = rightController.transform.rotation;
+			cylinder.transform.SetParent (rightController.transform);
+			cylinder.SetActive (true);
+            this.gameObject.SetActive(false);
+			Debug.Log ("Together");
+
+		}*/
+			
+	}
+		
+}
